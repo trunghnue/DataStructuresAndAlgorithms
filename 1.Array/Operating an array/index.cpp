527922a9b5433e4fb068ccaea5fd8cde29da0de4
Nguyễn Thành Trung <trungnt1.ctv@gmail.com>
2023-10-08 08:45:00 +0700

    add comment: Operating an array (array)

--- 1.Array/Operating an array/index.cpp
+++ 1.Array/Operating an array/index.cpp
@@ -102,3 +102,25 @@ bool deleteEle(int a[], int z)
     }
     return true;
 }
+
+// https://practice.geeksforgeeks.org/problems/operating-an-array/1
+
+// Given an array A of size N, your task is to do some operations, i.e., search an element x, insert an element y at index yi, and delete an element z by completing the functions. Also, all functions should return a boolean value.
+
+// Note:
+
+// In delete operation return true even if element is not present.
+// N is never greater than 10000.
+// Input Format:
+// N
+// A1 A2 . . . An
+// x y yi z
+
+// Example:
+// Input:
+// 5
+// 2 4 1 0 6
+// 1 2 2 0
+
+// Output:
+// 1 1 1
