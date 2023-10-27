1503a1f320768575c1ff7474d7f6755f249782d3
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-10-27 14:45:00 +0700

    add comment

--- 2.String/Palindrome/Palindrome String/index.cpp
+++ 2.String/Palindrome/Palindrome String/index.cpp
@@ -52,3 +52,19 @@ int main()
     return 0;
 }
 // } Driver Code Ends
+
+// https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+// Given a string S, check if it is palindrome or not.
+
+// Example 1:
+
+// Input: S = "abba"
+// Output: 1
+// Explanation: S is a palindrome
+// Example 2:
+
+// Input: S = "abc"
+// Output: 0
+// Explanation: S is not a palindrome
+// Your Task:
+// You don't need to read input or print anything. Complete the function isPalindrome()which accepts string S and returns an integer value 1 or 0.
