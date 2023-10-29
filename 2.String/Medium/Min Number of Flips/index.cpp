317987ae4b663da208c0d0a2e8828cfcdf2ef10c
Nguyễn Thành Trung <trungnt1.ctv@gmail.com>
2023-10-29 16:59:32 +0700

    add comment

--- 2.String/Medium/Min Number of Flips/index.cpp
+++ 2.String/Medium/Min Number of Flips/index.cpp
@@ -60,3 +60,26 @@ int minFlips(string S)
 
     return min(result0, result1);
 }
+
+// https://practice.geeksforgeeks.org/problems/min-number-of-flips3210/1
+//  Given a binary string, that is it contains only 0s and 1s.
+//  We need to make this string a sequence of alternate characters by flipping some of the bits,
+//  our goal is to minimize the number of bits to be flipped.
+
+// Example 1:
+
+// Input:
+// S = "001"
+// Output: 1
+// Explanation:
+// We can flip the 0th bit to 1 to have
+// 101.
+
+// Example 2:
+
+// Input:
+// S = "0001010111"
+// Output: 2
+// Explanation: We can flip the 1st and 8th bit
+// bit to have "0101010101"
+// 101.
